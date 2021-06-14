88dbfef699f9573f4df4de30a5f6b2de483fc43b
Jannat21 <davood.jannat@gmail.com>
2021-06-14 09:12:06 -0700

    test 2

--- Test1.py
+++ Test1.py
@@ -8,7 +8,7 @@ if not mt5.initialize(login=41447387, server="MetaQuotes-Demo", password="oyzdut
     print("initialize() failed, error code =", mt5.last_error())
     quit()
 
-Symbol = "AUDJPY"
+Symbol = "XAUUSD"
 TimeFrame = mt5.TIMEFRAME_H1
 timezone = pytz.timezone("Etc/GMT-2")
 StartDate = datetime(2021, 6, 1, tzinfo=timezone)
